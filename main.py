8ea26c92c7b7a4bca4cd94a5172e5198a5473f8b
jrweiss01 <jrweiss01@gmail.com>
2019-05-29 07:36:35 +0300

    Adding support for the modes data/test and adding dictionaries to the DB

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import os
 
 
 @click.command()
+@click.option('--mode', '-m', default='test', help='test mode will execute tests and comparisons, '
+                                                   'data mode will run the analysis and provide the user with details'
+                                                   'without comparing the results to a baseline')
 @click.option('--checkpoint', '-c', default='fast',
               help='the name of the checkpoint model to use when processing the images')
 @click.option('--save', '-s', default=False, help='Save the image with bounding boxes or not')
 @click.option('--images', '-i', multiple=True, default=[], help='list of full paths to images that should be processed')
-def main(checkpoint, save, images):
-    """This module requires at least one image (-i) in the comand line.
+def main(mode, checkpoint, save, images):
+    """This module requires at least one image (-i) in the command line.
     The module can handle many images in the same run.
     Optional parameters include Checkpoint name (default is safe) Save will
     determine if the image should be saved along with the object bounding boxes."""
@@ -23,34 +26,36 @@ def main(checkpoint, save, images):
     if len(images) > 0:
         my_luminoth = Perception(images, save, checkpoint)
         objects = my_luminoth.run()
+        if mode == 'data':
+            print(objects)
+        else:
+            for image in images:
+                image_path, image_name = os.path.split(image)
+                objects[image_name].update({"Pass": True})
+                comparision = CompareDetections(image_name, objects[image_name]["objects"], DB.baseline_results)
+                accuracy_of_detections = comparision.compare_objects_to_baselines()
+                number_of_detections = comparision.evaluate_num_found_objects()
+                processing_time = comparision.evaluate_processing_time(objects[image_name]["detection_time"],
+                                                      DB.baseline_results[image_name]["detection_time"],
+                                                      threshold)
+                #  TODO: This should be written to a DB of results
+                objects[image_name]["Pass"] = False if not processing_time or \
+                                                         not number_of_detections or \
+                                                         not accuracy_of_detections else \
+                    objects[image_name]["Pass"]
+                if objects[image_name]["Pass"]:
+                    number_of_passed_tests += 1
+                else:
+                    number_of_failed_test += 1
+                    print("Image %s failed! Number of detections: %s, Processing time: %s, Accuracy of detections %s"
+                          % (image_name, "passed" if number_of_detections else "failed",
+                             "passed" if processing_time else "failed",
+                             "passed" if accuracy_of_detections else "failed"))
+                number_of_tests_run = number_of_passed_tests + number_of_failed_test
 
-        for image in images:
-            image_path, image_name = os.path.split(image)
-            objects[image_name].update({"Pass": True})
-            comparision = CompareDetections(image_name, objects[image_name]["objects"], DB.latest_results)
-            accuracy_of_detections = comparision.compare_objects_to_baselines()
-            number_of_detections = comparision.evaluate_num_found_objects()
-            processing_time = comparision.evaluate_processing_time(objects[image_name]["detection_time"],
-                                                  DB.latest_results[image_name]["detection_time"],
-                                                  threshold)
-            #  TODO: This should be written to a DB of results
-            objects[image_name]["Pass"] = False if not processing_time or \
-                                                     not number_of_detections or \
-                                                     not accuracy_of_detections else \
-                objects[image_name]["Pass"]
-            if objects[image_name]["Pass"]:
-                number_of_passed_tests += 1
-            else:
-                number_of_failed_test += 1
-                print("Image %s failed! Number of detections: %s, Processing time: %s, Accuracy of detections %s"
-                      % (image_name, "passed" if number_of_detections else "failed",
-                         "passed" if processing_time else "failed",
-                         "passed" if accuracy_of_detections else "failed"))
-            number_of_tests_run = number_of_passed_tests + number_of_failed_test
-
-            # print(sorted_objects)
-        print("%d Tests run;  %d PASSED, %d FAILED" %
-              (number_of_tests_run, number_of_passed_tests, number_of_failed_test))
+                # print(sorted_objects)
+            print("%d Tests run;  %d PASSED, %d FAILED" %
+                  (number_of_tests_run, number_of_passed_tests, number_of_failed_test))
     else:
         print(main.__doc__)
         pass
